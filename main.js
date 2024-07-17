b840f9d552a94501ff2fb41dcdfed87f098b0d57
SEVERNASTER <deniszeballos15@gmail.com>
2024-07-16 21:14:35 -0400

    first commit - responsive menu added

---
+++ main.js
@@ -0,0 +1,45 @@
+
+var mvScreenIsVisible = false;
+var menuResponsiveIsVisible = false;
+const menu = document.getElementById('menu');
+const homeElements = document.querySelectorAll('.home');
+
+
+document.getElementById('mission-vision').addEventListener('click', function() {
+    const mvScreen = document.getElementById('mvscreen');
+    mvScreen.classList.add('active');//innecesario, podemos cambiar solamente el estilo style.opacity
+    document.getElementById('mvcba').classList.add('active');
+});
+
+document.getElementById('x-button').addEventListener('click', closeMVScreen)
+
+function closeMVScreen(){
+    const mvScreen = document.getElementById('mvscreen');
+    mvScreen.classList.remove('active');
+    document.getElementById('mvcba').classList.remove('active');
+}
+
+document.getElementById('menu-icon').addEventListener('click', function() {
+    if(!menuResponsiveIsVisible){
+        menu.style.opacity = '1';
+        menuResponsiveIsVisible = true;
+    }else{
+        menu.style.opacity = '0';
+        menuResponsiveIsVisible = false;
+    }
+} )
+
+
+// code to desappear the responsive menu by touching any other part of the home screen
+// homeElements.forEach(element => {
+//     element.addEventListener('click', function(event) {
+//         event.preventDefault();
+
+//         if(!(element.id === 'menu-icon')){
+//             // menu.style.opacity = '0';
+//             // menuResponsiveIsVisible = false;
+//             console.log('activado');
+//         }
+//     })
+// })
+
