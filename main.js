ef897d5a170f1fef445134a1159085cb0636cd89
SEVERNASTER <deniszeballos15@gmail.com>
2024-07-23 21:09:35 -0400

    still working on responsive design

--- main.js
+++ main.js
@@ -29,12 +29,11 @@ document.getElementById('menu-icon').addEventListener('click', function() {
     }
 } )
 
-// para poner mv screen responsive
-// document.getElementById('mission-vision-responsive').addEventListener('click', function() {
-//     const mvscreen = document.getElementById('mvscreen');
-//     mvscreen.style.opacity = '1';
-//     mvscreen.style.zIndex = '30';
-// })
+document.getElementById('mission-vision-responsive').addEventListener('click', function() {
+    const mvscreen = document.getElementById('mvscreen');
+    mvscreen.style.opacity = '1';
+    mvscreen.style.zIndex = '30';
+})
 
 
 // code to desappear the responsive menu by touching any other part of the home screen
